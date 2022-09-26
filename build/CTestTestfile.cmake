95a126648e5e51a087f2cf0cfa249a8c95cfab66
ma-2pac <marcoalec@gmail.com>
2022-09-26 12:21:32 +1000

    run catkin make setup

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/marco/Documents/Github/MTRX3760_PROJ1/src
+# Build directory: /home/marco/Documents/Github/MTRX3760_PROJ1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
