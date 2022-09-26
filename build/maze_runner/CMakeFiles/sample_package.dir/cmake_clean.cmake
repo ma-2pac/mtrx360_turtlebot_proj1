c135719889b6fe32ab69bb6753cfceb447fadf38
ma-2pac <marcoalec@gmail.com>
2022-09-26 16:52:38 +1000

    attempt to run sample_package node on physical turtlebot

---
+++ build/maze_runner/CMakeFiles/sample_package.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/marco/Documents/Github/MTRX3760_PROJ1/devel/lib/maze_runner/sample_package"
+  "/home/marco/Documents/Github/MTRX3760_PROJ1/devel/lib/maze_runner/sample_package.pdb"
+  "CMakeFiles/sample_package.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample_package.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
